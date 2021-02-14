e50f2dc7305d6c736f1d2b45a5160db462d78765
Mike Shohet <mlshohet@gmail.com>
2021-02-14 02:29:56 -0500

    react native workshop

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.screen }>
+      <View style={ styles.inputContainer }>
+        <TextInput 
+          placeholder={"Shopping List"}
+          style={styles.input}
+        />
+        <Button
+          title="ADD"
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
+
+  inputContainer: {
+    flexDirection: 'column', 
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderBottomWidth: 1,
+    padding: 10
+  }
 });
+
+
+
+
